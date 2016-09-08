c9d6fa01b01111846ea7d05bc284dd0713ac9b7d
Kevin Cai <kcai@usc.edu>
2016-09-07 18:48:30 -0700

    Initial Commit

---
+++ Nooma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nooma
+//
+//  Created by Kevin Cai on 9/7/16.
+//  Copyright © 2016 Kevin Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
